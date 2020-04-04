f28aeebc4a0174c1966ead4c7289818864a89a53
Erica Schwartz <erica.schwartz.4@gmail.com>
2020-04-04 12:57:43 -0700

    Create random_or_override.py

---
+++ random_or_override.py
@@ -0,0 +1,27 @@
+import random
+
+"""
+Returns either a random int, or an overriden value (for testing).
+When overriding, pass in a sequence of ints for this function to return.
+"""
+class RandomOrOverride:
+  def __init__(self, override=[]):
+    self.override = override
+    self.override_index = 0
+    random.seed()
+
+  def random_range(self, min, max):
+    if self.override_index >= len(self.override):
+      return random.randint(min, max)
+    else:
+      res = self.override[self.override_index]
+      self.override_index += 1
+      return res
+
+  def random_sample(self, population, k):
+    if self.override_index + k > len(self.override):
+      return random.sample(population, k)
+    else:
+      res = self.override[self.override_index:self.override_index + k]
+      self.override_index += k
+      return res
