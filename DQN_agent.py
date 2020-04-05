41a2806b7fd9308df81b0433cfcaecba46901cad
wert23239 <perfectkirby2339@gmail.com>
2020-04-05 00:20:21 -0700

    Show both model results and only load model on save

--- DQN_agent.py
+++ DQN_agent.py
@@ -4,7 +4,7 @@ from collections import deque
 from keras import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
-from numpy import argmax,array
+from numpy import argmax, array
 
 from action import Action
 from constants import NUMBER_OF_CIRCLES, NUMBER_OF_COLORS, NUMBER_OF_ROWS
@@ -12,57 +12,60 @@ from random_or_override import RandomOrOverride
 
 STATE_SPACE = 185
 ACTION_SPACE = 180
-BATCH_SIZE = 64
+BATCH_SIZE = 128
 TRAIN_AMOUNT = 5
-EPOCHS = 3
+EPOCHS = 1
 
 
-class DQNAgent():
-    def __init__(self, random_or_override):
+class DQNAgent:
+    def __init__(self, random_or_override, human=False):
         self.random_or_override = random_or_override
-        self.memory = deque(maxlen=2000)
-        self.discount_factor = .95
+        self.memory = deque(maxlen=5000)
+        self.discount_factor = 0.85
         # exploration vs. exploitation  params
         self.epsilon = 1.0
-        self.epsilon_min = .01
-        self.epsilon_decay = 0.995
-        self.learning_rate = 0.01
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.999
+        self.learning_rate = 0.005
         self.train_count = 0
 
+        self.tau = 0.125
 
         self.model = self.__create_model()
-        self.model.load_weights('DQN_weights.h5')
+        if human:  # Add check for weights file exisiting
+            self.model.load_weights("DQN_complete_weights.h5")
         self.target_model = self.__create_model()
 
-    def action(self,state,possible_actions,_,train):
+    def action(self, state, possible_actions, _, train):
         self.epsilon = max(self.epsilon_min, self.epsilon)
         if train and self.random_or_override.random_range_cont() < self.epsilon:
             l = list(possible_actions)
-            return (self.random_or_override.random_sample(l, 1)[0],0,-1)
+            return (self.random_or_override.random_sample(l, 1)[0], 0, -1)
         observable_state = state.to_observable_state()
-        return self.__get_best_possible_action(self.model.predict(array([observable_state]))[0],possible_actions)
+        return self.__get_best_possible_action(
+            self.model.predict(array([observable_state]))[0], possible_actions
+        )
 
-    def __get_best_possible_action(self,prediction,possible_actions):
+    def __get_best_possible_action(self, prediction, possible_actions):
         prediction_list = prediction.argsort().tolist()
         wrong_guesses = 0
-        while(len(prediction_list)):
+        while len(prediction_list):
             action_number = prediction_list.pop()
             action = self.__convert_action_num(action_number)
             if action in possible_actions:
-                return (action,action_number,wrong_guesses)
+                return (action, action_number, wrong_guesses)
             wrong_guesses += 1
         raise Exception
 
-    def __convert_action_num(self,action_number): #TEST
-        circle = action_number // (NUMBER_OF_COLORS*NUMBER_OF_ROWS)
-        action_number = action_number % (NUMBER_OF_COLORS*NUMBER_OF_ROWS)
+    def __convert_action_num(self, action_number):  # TEST
+        circle = action_number // (NUMBER_OF_COLORS * NUMBER_OF_ROWS)
+        action_number = action_number % (NUMBER_OF_COLORS * NUMBER_OF_ROWS)
         color = action_number // (NUMBER_OF_ROWS) + 1
         row = action_number % NUMBER_OF_ROWS
-        return Action(circle,color,row)
+        return Action(circle, color, row)
 
-    def save(self,example):
+    def save(self, example):
         self.memory.append(example)
-
 
     def train(self):
         self.epsilon *= self.epsilon_decay
@@ -77,30 +80,36 @@ class DQNAgent():
             if sample.done:
                 target[0][example.action] = example.reward
             else:
-                prediction = self.target_model.predict(array([example.next_state]))[0]
-                action = self.__get_best_possible_action(prediction,example.possible_actions)[1]
+                prediction = self.target_model.predict(
+                    array([example.next_state]))[0]
+                action = self.__get_best_possible_action(
+                    prediction, example.possible_actions
+                )[1]
                 Q_future = prediction[action]
                 reward = example.reward + Q_future * self.discount_factor
                 target[0][example.action] = reward
-            self.model.fit(array([example.state]),target, epochs=EPOCHS, verbose=0)
+            self.model.fit(array([example.state]),
+                           target, epochs=EPOCHS, verbose=0)
         if self.train_count % TRAIN_AMOUNT == 0:
             self.__target_train()
-            print("epilson",self.epsilon)
-            self.model.save_weights('DQN_weights.h5')
-            self.model.save_weights('DQN_target_weights.h5')
+            print("epilson", self.epsilon)
+            self.model.save_weights("DQN_weights.h5")
+            self.model.save_weights("DQN_target_weights.h5")
 
     def __target_train(self):
         weights = self.model.get_weights()
         target_weights = self.target_model.get_weights()
         for i in range(len(target_weights)):
-            target_weights[i] = weights[i]
+            target_weights[i] = weights[i] * self.tau + target_weights[i] * (
+                1 - self.tau
+            )
         self.target_model.set_weights(target_weights)
 
     def __create_model(self):
         model = Sequential()
-        model.add(Dense(24, input_dim=STATE_SPACE, activation='relu'))
+        model.add(Dense(24, input_dim=STATE_SPACE, activation="relu"))
         model.add(Dense(48, activation="relu"))
-        model.add(Dense(24, activation='relu'))
+        model.add(Dense(24, activation="relu"))
         model.add(Dense(ACTION_SPACE))
         model.compile(loss="mean_squared_error",
                       optimizer=Adam(lr=self.learning_rate))
